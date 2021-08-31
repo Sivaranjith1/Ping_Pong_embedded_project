1b777c32f27aace28a04bdf2be1b7a7373e9d3f4
Sivaranjith Sivarasa <ranjith1210000@gmail.com>
2021-08-31 21:26:46 +0200

    Fixed latch?

--- g12_ATmega162/main.c
+++ g12_ATmega162/main.c
@@ -14,18 +14,20 @@ int main(void)
 {
   usart_init(&usart_instance_0, MAX233_BAUDRATE);
   FILE* output = fdevopen(usart_putchar, 0);
-  
   uint16_t address = 0x2000;
-  unsigned char *p = (unsigned char *) (address + 1);
-  
-  DDRC = 0xFF;
-  PORTC = 0x00;
-  
-  SFIOR = (1 << XMM1) | (1 << XMM0);
-  
-  *p = 0xaa;
-  
-  SFIOR = 0x00;
-  *p = 0x55;
+  while(1){
+	  MCUCR |= (1 << SRE);
+	  unsigned char *p = (unsigned char *) (address + 1);
+	  DDRC = 0xFF;
+	  PORTC = 0x00;
+	  SFIOR = (1 << XMM1) | (1 << XMM0);
+	  *p = 0xaa;
+	  
+	  SFIOR = 0x00;
+	  
+	  *p = 0x55;
+	  
+	  MCUCR = 0x00;
+  };
 }
 
