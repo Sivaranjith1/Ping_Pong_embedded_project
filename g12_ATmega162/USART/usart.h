4284e01f181b53a12e30c09f2ba0784b104d0641
Sivaranjith Sivarasa <ranjith1210000@gmail.com>
2021-08-31 16:41:17 +0200

    Added doxygen comment to usart.h

--- g12_ATmega162/USART/usart.h
+++ g12_ATmega162/USART/usart.h
@@ -1,9 +1,13 @@
-/*
- * usart.h
- *
- * Created: 31.08.2021 10:57:19
- *  Author: Sivaranjith Sivarasa
- */ 
+/**
+ * @file usart.h
+ * @author Sivaranjith Sivarasa
+ * @brief USART driver for ATMEGA162
+ * @version 0.1
+ * @date 2021-08-31
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #ifndef USART_H_
 #define USART_H_
 
@@ -16,6 +20,10 @@
 #define USART_UBRR(BAUD) ((F_CPU/10)/(1.6*BAUD) - 1)
 #define PRINT_F_USART &usart_instance_0
 
+/**
+ * @brief A usart instance and its corresponding registors. This is used to choose with instance to read/send from 
+ * 
+ */
 typedef struct {
     volatile uint8_t* udr;
     volatile uint8_t* ucsra;
@@ -25,16 +33,34 @@ typedef struct {
     volatile uint8_t* ubrrh;
 } usart_instance_t;
 
-
+//Usart instances corresponding to the instances in the AVR datasheet
 extern usart_instance_t usart_instance_0;
 extern usart_instance_t usart_instance_1;
 
+/**
+ * @brief initializing usart instance for async clock and given
+ * 
+ * @param usart_instance One of the ATMEGA162 usart instances with its registors
+ * @param baudrate baudrate to use on the usart instance
+ */
 void usart_init(usart_instance_t* usart_instance, uint16_t baudrate);
 
+/**
+ * @brief Transmit a character on the usart instance
+ * 
+ * @param usart_instance Usart instance to use for transmission
+ * @param data The character to transmit
+ */
 void usart_transmit(usart_instance_t* usart_instance, unsigned char data);
 
 unsigned char usart_receive(void);
 
+/**
+ * @brief Transmission for printf function
+ * 
+ * @param c the character to transmit
+ * @return int 0 if transmission was a success
+ */
 int usart_putchar(char c, FILE* stream);
 
 
