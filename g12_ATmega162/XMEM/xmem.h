0cef102081b33f81d7a329276d4723c0cc880eb3
Sivaranjith Sivarasa <ranjith1210000@gmail.com>
2021-09-07 13:16:56 +0200

    Added comments to xmem interface

--- g12_ATmega162/XMEM/xmem.h
+++ g12_ATmega162/XMEM/xmem.h
@@ -1,20 +1,45 @@
-/*
- * IncFile1.h
- *
- * Created: 07.09.2021 12:47:38
- *  Author: sivarans
- */ 
+/**
+ * @file xmem.h
+ * @author Steffen Folåsen
+ * @brief Library for interfacing with AVR's external memory, xmem
+ * @version 0.1
+ * @date 2021-09-07
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include <stdint.h>
 
 #ifndef XMEM_H_
 #define XMEM_H_
 
+/**
+ * @brief Enable AVR interface for external memory (xmem)
+ * 
+ */
 void xmem_init(void);
 
-void xmem_write(uint8_t data, uint16_t adress);
+/**
+ * @brief Write to the external memory
+ * 
+ * @param data byte of data to write
+ * @param addr Adress of external memory for 0x1000. I.e will 0x1800 in absolut result in 0x800 as adress
+ */
+void xmem_write(uint8_t data, uint16_t addr);
 
+/**
+ * @brief Read a byte from external memory
+ * 
+ * @param addr Adress of external memory for 0x1000. I.e will 0x1800 in absolut result in 0x800 as adress
+ * @return uint8_t The byte of data stored in this memory
+ */
 uint8_t xmem_read(uint16_t addr);
 
 
+/**
+ * @brief Run a SRAM test. This will loop through every function in the sram memory space and validate that the input and output data is the same
+ * 
+ */
+void xmem_SRAM_test(void);
 
 #endif /* XMEM_H_ */
