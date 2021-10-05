3b0f540a19f08618d9046629561820d44b32a8d5
Sivaranjith Sivarasa <ranjith1210000@gmail.com>
2021-10-05 14:19:40 +0200

    Added new prosject for arduino

---
+++ g12_ATsam3X8E/g12_ATsam3X8E/main.c
@@ -0,0 +1,21 @@
+/*
+ * g12_ATsam3X8E.c
+ *
+ * Created: 05.10.2021 14:18:42
+ * Author : sivarans
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
